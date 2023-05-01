1bcdea56d6614a8862cfe4c5e5eb320c62c7b963
tristan <tristanaaron.2711@gmail.com>
2023-05-01 14:29:35 +0700

    Added Artist Selection

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const player = document.querySelector('.bi-arrows-angle-expand');
+const artist = document.querySelector('.artist-overlay');
+const playButton = document.querySelector('.play-button');
+const longplayButton = document.querySelector('.long-play-button');
+
+
 player.addEventListener('click', () => {
 	window.location.href = 'play/play.html';
 });
 
-
-const playButton = document.querySelector('.play-button');
-const longplayButton = document.querySelector('.longplay-button');
-
+artist.addEventListener('click', () => {
+	artist.classList.toggle ('clicked');
+});
 
 playButton.addEventListener('click', () => {
   playButton.classList.toggle('clicked');
